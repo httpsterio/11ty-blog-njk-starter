8bddb6eabf5daecd6f867fe3854b2ae42e436746
Sami Mäkelä <sami@httpster.io>
2022-05-15 15:35:18 +0300

    broken

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   darkMode: 'class',
-  content: ['./src/**/*.md', './src/**/*.html', './src/_includes/**/*.liquid'],
+  content: ['./src/**/*.md', './src/**/*.html', './src/_includes/**/*.njk'],
   plugins: [
     require('@tailwindcss/typography')
   ],
